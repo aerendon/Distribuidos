4fc8f4d3697ef452782bfacf2a0b4b322430b8dd
Alejandro E. Rendon <alejorendon@utp.edu.co>
2016-10-06 00:21:08 -0500

    Grandulon

---
+++ Proyecto I/Grandulon/server1.py
@@ -0,0 +1,24 @@
+import sys
+import time
+import os
+from socket import *
+
+host = "127.0.0.1" # set to IP address of target computer
+port = [4000, 5000]
+id_host = "2"
+buf = 1024
+UDPSock = socket(AF_INET, SOCK_DGRAM)
+addr_self = (host, 3000)
+UDPSock.bind(addr_self)
+
+for i in port:
+    addr = (host, i)
+    UDPSock.sendto(id_host, addr)
+    UDPSock.sendto("3000", addr)
+    print ("Enviado id "+ id_host + " a " + str(addr[1]))
+    (id_host, addr) = UDPSock.recvfrom(buf)
+    print ("id de port " + str(i) + " ... " +id_host)
+
+
+UDPSock.close()
+os._exit(0)
