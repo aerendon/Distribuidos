07f1482b73721c8137923dd835d32412c4b97f49
Alejandro E. Rendon <alejorendon@utp.edu.co>
2016-10-26 23:43:19 -0500

    Finalizado taller 7

--- 7. Inactividad BD/client1.py
+++ 7. Inactividad BD/client1.py
@@ -7,28 +7,18 @@ host = "127.0.0.1" # set to IP address of target computer
 buf = 1024
 UDPSock = socket(AF_INET, SOCK_DGRAM)
 
-addr_self = (host, 3000)
+addr_self = (host, 5000)
 UDPSock.bind(addr_self)
 
 addr_server = (host, 4000)
 signal = "1"
 
 while True:
-  UDPSock.sendto("3000", addr_server)
+  UDPSock.sendto("5000", addr_server)
   (process, addr_self) = UDPSock.recvfrom(buf)
   print "Tiene un proceso de " + process + "Seg"
 
   time.sleep(int(process))
-
-
-
-# for i in port:
-#     addr = (host, i)
-#     UDPSock.sendto(id_host, addr)
-#     UDPSock.sendto("3000", addr)
-#     print ("Enviado id "+ id_host + " a " + str(addr[1]))
-#     (id_host, addr) = UDPSock.recvfrom(buf)
-#     print ("id de port " + str(i) + " ... " +id_host)
 
 
 UDPSock.close()
--- 7. Inactividad BD/server.py
+++ 7. Inactividad BD/server.py
@@ -5,17 +5,36 @@ from socket import *
 
 host = "127.0.0.1" # set to IP address of target computer
 buf = 1024
+id_host = []
+process_host = []
 UDPSock = socket(AF_INET, SOCK_DGRAM)
 addr_self = (host, 4000)
 UDPSock.bind(addr_self)
 
+process = [10, 2, 5, 15, 3, 7, 8, 1]
+process_act = 0
 
-# (id_rec, addr_self) = UDPSock.recvfrom(buf)
-# (host_rec, addr_self) = UDPSock.recvfrom(buf)
-# addr = (host, int(host_rec))
-# print (addr)
-# UDPSock.sendto (str(max(int(id_host), int(id_rec))), addr)
-# print ("id " + str(max(int(id_host), int(id_rec)))+ " enviado...")
+while process_act < len(process):
+  # addr = (host, i)
+  (host_rec, addr_self) = UDPSock.recvfrom(buf)
+  UDPSock.sendto(str(process[process_act]), (host, int(host_rec)))
+  if host_rec in id_host:
+    print "aqui"
+    ind = id_host.index(host_rec)
+    process_host[ind] = str(process[process_act])
+  else:
+    print "Wlse"
+    id_host.append(host_rec)
+    process_host.append(str(process[process_act]))
+
+  print "____LISTA"
+  for i in range(len(id_host)):
+    print id_host[i] + "-->" + process_host[i]
+  print "____"
+
+  process_act += 1
+
+  # print host_rec
 
 UDPSock.close()
 os._exit(0)
